e9703bfc0e88bae379944a02e408ab6e20bff440
Khanh26 <trongkhanh2014@gmail.com>
2022-02-19 16:21:18 +0700

    update footer - done

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link rel="stylesheet" href="http://localhost/santmdt/assets/css/style.css">
-    <title>Document</title>
+    <title>Trang chủ - Sàn Thương mại số 1 Việt Nam</title>
 </head>
 
 <body>
@@ -14,7 +14,7 @@
         <!-- header -->
         <?php include('./view/layout/client/header.php'); ?>
         <!-- End header -->
-        
+
         <!-- footer -->
         <?php include('./view/layout/client/footer.php');?>
         <!-- End footer -->
--- view/layout/client/footer.php
+++ view/layout/client/footer.php
@@ -1,112 +1,70 @@
 <div class="footer">
-    <div class="footer_logo">
-        <div class="footer_icon_1">
-            <p><b>HOTLINE</b></p>
-            <div class="mauchu">
-            <p><b>0939.937.886</b></p>
-            </div>
-        </div>
-
-        <div class="footer_icon_2">
-            <p><b>Kết nối với chúng tôi</b></p>
-                
-                <i class="fab fa-facebook-square" style="font-size:35px;color:blue"></i>
-
-                <i class="fab fa-twitter-square" style="font-size:35px;color:blue"></i>
-
-                <i class="fab fa-google-plus-square" style="font-size:35px;color:red"></i>
-
-                <i class="fab fa-instagram" style="font-size:36px;color:red"></i>
-
-        </div>
-
-        <div class="footer_icon_3">
-            <p><b>App mua bán</b></p>
-            <p>
-                <img src="http://localhost/santmdt/assets/img/footer/logo-appstore.png">
-            </p>
-            <p>
-                <img src="http://localhost/santmdt/assets/img/footer/logo-googleplay.png">
-            </p>
-        </div>
-
-        <div class="footer_icon_4">
-            <p><b>Đăng ký nhận bản tin khuyến mãi</b></p>
-            <form action="">
-                <input list="browsers" name="browser">
-                <datalist id="browsers">
-                </datalist>
-                <input type="submit">
-                </form>
-        </div>
-    </div>
-
     <div class="footer_cobct">
-    <div class="footer1">
-        <p><b>KÊNH PHÂN PHỐI</b></p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Nhà phân phối
-        </p>
-        <p><img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Chi nhánh cửa hàng
-        </p>
-        <p>
-            <img class src="http://localhost/santmdt/assets/img/footer/dkbct.png" width="182" height="69">
-        </p>			
-    </div>
-
-    <div class="footer2">
-        <p><b>TRỢ GIÚP</b></p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Hướng dẫn mua hàng
-        </p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Phương thức thanh toán
-        </p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Chính sách đổi trả và hoàn tiền
-        </p>
-    </div>
-
-    <div class="footer3">
-        <p><b>SẢN PHẨM</b></p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Dinh dưỡng cây trồng</p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Dụng cụ sân vườn</p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Hạt giống cây trồng</p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-list-footer.png">
-            Thuốc bảo vệ thực vật</p>
-    </div>
-
-    <div class="footer4">
-        <p><b>VĂN PHÒNG CHÍNH</b></p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-home.png">
-            Đại Học Cần Thơ
-        </p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-phone.png">
-            <span>0939.937.886</span>
-        </p>
-        <p>
-            <img src="http://localhost/santmdt/assets/img/footer/icon-mail.png">
-            thegioivattunongnghiep@gmail.com
-        </p>
-    </div>
-    </div>
-    <div class="footer_cuoicung">
-        <div class="">
-            © 2021 Sàn Thương Mại Điện Tử Vật Tư Nông Nghiệp.
+        <div class="item-footer">
+            <h3 class="heading-footer">Kết nối với chúng tôi</h3>
+                <a href="" class="link-footer social-footer" title="facebook">
+                    <i class="fab fa-facebook-square" style="color:blue;"></i>
+                </a>
+                <a href="" class="link-footer social-footer" title="twitter">
+                    <i class="fab fa-twitter-square" style="color:blue;"></i>
+                </a>
+                <a href="" class="link-footer social-footer" title="google+">
+                    <i class="fab fa-google-plus-square" style="color:red;"></i>
+                </a>
+                <a href="" class="link-footer social-footer" title="instagram">
+                    <i class="fab fa-instagram" style="color:red;"></i>
+                </a>
         </div>
+
+        <div class="item-footer">
+            <h3 class="heading-footer">TRỢ GIÚP</h3>
+            <a href="" class="link-footer">
+                <i class="fas fa-arrow-right icon-footer"></i>
+                Hướng dẫn mua hàng
+            </a>
+            <a href="" class="link-footer">
+                <i class="fas fa-arrow-right icon-footer"></i>
+                Phương thức thanh toán
+            </p>
+            <a href="" class="link-footer">
+                <i class="fas fa-arrow-right icon-footer"></i>
+                Chính sách đổi trả và hoàn tiền
+            </a>
+        </div>
+            <!-- loại san phẩm -->
+        <div class="item-footer">
+            <h3 class="heading-footer">SẢN PHẨM</h3>
+            <a href="" class="link-footer">
+                <i class="fas fa-arrow-right icon-footer"></i>
+                Dinh dưỡng cây trồng</a>
+            <a href="" class="link-footer">
+                <i class="fas fa-arrow-right icon-footer"></i>
+                Dụng cụ sân vườn</a>
+            <a href="" class="link-footer">
+                <i class="fas fa-arrow-right icon-footer"></i>
+                Hạt giống cây trồng</a>
+            <a href="" class="link-footer">
+                <i class="fas fa-arrow-right icon-footer"></i>
+                Thuốc bảo vệ thực vật</a>
+        </div>
+
+        <div class="item-footer">
+            <h3 class="heading-footer">VĂN PHÒNG CHÍNH</h3>
+            <p href="" class="content-footer">
+                <i class="fas fa-home icon-footer"></i>
+                Đại Học Cần Thơ
+            </p>
+            <p href="" class="content-footer">
+            <i class="fas fa-phone-alt icon-footer"></i>
+                <span>0939.937.886</span>
+            </p>
+            <p href="" class="content-footer">
+                <i class="fas fa-envelope icon-footer"></i>
+                thegioivattunongnghiep@gmail.com
+            </p>
+        </div>
+    </div>
+    <div class="footer_end">
+        © 2021 Sàn Thương Mại Điện Tử Vật Tư Nông Nghiệp.
     </div>
 </div>
