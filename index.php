eb06354019e58682a87b77d2854bf4366ffed91f
unknown <trongkhanh2014@gmail.com>
2022-02-19 15:05:56 +0700

    create footer - Thinh

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
+    <link rel="stylesheet" href="http://localhost/santmdt/assets/css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="wapper">
+        <!-- header -->
+        <?php include('./view/layout/client/header.php'); ?>
+        <!-- End header -->
+
+        <!-- footer -->
+        <?php include('./view/layout/client/footer.php');?>
+        <!-- End footer -->
+    </div>
+
+</body>
+</html>
