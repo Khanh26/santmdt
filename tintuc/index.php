aeee61653ff8c8bee1c800adfcfa50ed4c74101a
thinh0910 <doanphuocthinh0910@gmail.com>
2022-02-22 01:30:23 +0700

    Meow

--- tintuc/index.php
+++ tintuc/index.php
@@ -28,18 +28,21 @@
         <h1><center>Kỹ Thuật Nông Nghiệp</center></h1>
         <br>
         <br>
-        
+
         <div class="clearfix">
             <h2>Cách Bón Phân Cho Cây Hoa Mai Vàng</h2>
+            <br>
             <p class="cover img1"><img src="http://localhost/santmdt/assets/img/hoa-mai-vang.jpg" alt="" style="width:300px;height:200px"></p>
             <br>
             <br>
+            <div class="">
             &emsp;Mai có xuất xứ từ cây hoang dại, 
             có khả năng thích ứng tốt với điều kiện khí hậu nhiệt đới, 
             đặc biệt với khí hậu miền Nam. Cây mai sinh trưởng và phát triển mạnh, 
             có tuổi thọ cao và nếu được chăm sóc chu đáo sẽ cho hoa nhiều và có màu sắc đẹp. 
             Cây mai mỗi năm rụng lá một lần vào cuối mùa Đông (tháng 1 – tháng 2 Dương lịch) 
             và nở hoa vào đầu mùa Xuân, chỉ riêng mai Tứ Quý là nở hoa quanh năm.
+            </div>
             <a href="http://localhost/santmdt/tintuc/html/hoamai.php">
                 <br>
                 Xem Thêm</a>
@@ -47,10 +50,13 @@
                 <br>
             <p>&emsp;22/02/2022</p>
         </div>
+        <br>
+        <hr>
         <hr>
 
         <div class="clearfix">
-        <h2>Giới thiệu quy trình trồng xoài, hướng dẫn chăm sóc cây xoài</h2>
+            <h2>Giới thiệu quy trình trồng xoài, hướng dẫn chăm sóc cây xoài</h2>
+            <br>
             <p class="cover img1"><img src="http://localhost/santmdt/assets/img/cay-xoai-6.jpg" alt="" style="width:300px;height:200px"></p>
             <br>
             <br>
@@ -67,10 +73,13 @@
                 <br>
             <p>&emsp;22/02/2022</p>
         </div>
+        <br>
+        <hr>
         <hr>
 
         <div class="clearfix">
             <h2>Quy trình trồng và chăm sóc cây cà phê đúng kỹ thuật</h2>
+            <br>
             <p class="cover img1"><img src="http://localhost/santmdt/assets/img/ky_thuat_trong_ca_phe.jpg" alt="" style="width:300px;height:200px"></p>
             <br>
             <br>
@@ -87,7 +96,10 @@
                 <br>
             <p>&emsp;22/02/2022</p>
         </div>
+        <br>
         <hr>
+        <hr>
+        
         </div>
         <!-- End content -->
 
