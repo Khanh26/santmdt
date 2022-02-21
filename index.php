d8336cfd7101dc9b136142acd03e94100967aa25
duy1221 <duyb1805682@student.ctu.edu.vn>
2022-02-22 02:51:07 +0700

    tui buon ngu qua may ban oi, nua lam tiep. tui di ngu nha

--- index.php
+++ index.php
@@ -6,6 +6,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
     <link rel="stylesheet" href="http://localhost/santmdt/assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/santmdt/assets/css/home.css">
+    <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <title>Trang chủ - Sàn Thương mại số 1 Việt Nam</title>
 </head>
 
@@ -17,7 +19,160 @@
 
         <!-- Content -->
             <!-- Code trang chu o day -->
-            a
+        <div id="wrap">
+            <div id="content0">
+                <div id="cnt0-1">
+                    <div class="w3-content w3-display-container" id="slideshow">
+                        <img style="width: 100%; height: 100%; border-radius: 5px;" class="mySlides" src="https://www.phanbonnauy.com/resource/images/2020/06/32277650403c075dde0a2061bb8fbb62.jpg">
+                        <img style="width: 100%; height: 100%; border-radius: 5px;" class="mySlides" src="https://camau.gov.vn/wps/wcm/connect/2be19436-d79d-48c0-8260-c77e093ba286/a12.png?MOD=AJPERES&CVID=">
+                        <img style="width: 100%; height: 100%; border-radius: 5px;" class="mySlides" src="https://viettrunggroup.com/wp-content/grand-media/image/Actaone-247WP.jpg">
+                        <img style="width: 100%; height: 100%; border-radius: 5px;" class="mySlides" src="https://noma.vn/wp-content/uploads/2021/04/lam-vuon-min.jpg">
+                        <button class="w3-button w3-black w3-display-left" onclick="plusDivs(-1)">&#10094;</button>
+                        <button class="w3-button w3-black w3-display-right" onclick="plusDivs(1)">&#10095;</button>
+                    </div>
+                    <script>
+                        var slideIndex = 1;
+                        showDivs(slideIndex);
+                        function plusDivs(n) {
+                         showDivs(slideIndex += n);
+                        }
+                        function showDivs(n) {
+                            var i;
+                            var x = document.getElementsByClassName("mySlides");
+                            if (n > x.length) {slideIndex = 1}
+                            if (n < 1) {slideIndex = x.length}
+                            for (i = 0; i < x.length; i++) {
+                                x[i].style.display = "none";  
+                            }
+                            x[slideIndex-1].style.display = "block";  
+                        }
+                    </script>
+                    <!-- <div style="text-align:center">
+                        <span class="dot"></span> 
+                        <span class="dot"></span> 
+                        <span class="dot"></span> 
+                    </div>
+                    <script>
+                        var slideIndex = 0;
+                        showSlides();
+
+                        function showSlides() {
+                        var i;
+                        var slides = document.getElementsByClassName("mySlides");
+                        var dots = document.getElementsByClassName("dot");
+                        for (i = 0; i < slides.length; i++) {
+                            slides[i].style.display = "none";  
+                        }
+                        slideIndex++;
+                        if (slideIndex > slides.length) {slideIndex = 1}    
+                        for (i = 0; i < dots.length; i++) {
+                            dots[i].className = dots[i].className.replace(" active", "");
+                        }
+                        slides[slideIndex-1].style.display = "block";  
+                        dots[slideIndex-1].className += " active";
+                        setTimeout(showSlides, 1000); // Change image every 2 seconds
+                        }
+                    </script> -->
+
+                </div>
+                <div id="cnt0-2">
+                    <div id="cnt02a">
+                        <!-- <img style="width: 100%; height: 100%; border-radius: 5px;"  src="https://cf.shopee.vn/file/47b26a12d96788aebb0d9c5e6ecaa7c8"> -->
+                    </div>
+                    <div id="cnt02b">
+                        <!-- <img style="width: 100%; height: 100%; border-radius: 5px;" src="https://magiamgiashopee.com/wp-content/uploads/2019/05/sieu-giam-gia-shopee.jpg" > -->
+                    </div>
+                </div>
+            </div>
+            <div id="content1">
+                <div id="category">
+                   <b>DANH MỤC SẢN PHẨM</b> 
+                </div>
+                <div id="product">
+                    <div class="prod">
+                        <div class="prd prd-1">
+                            <div class="ctg_i">
+                                <img style="width: 100%; height: 100%; border-radius: 5px;" src="https://product.hstatic.net/1000220686/product/biorepel_toi_toi_361aa37a22e742f3b293c8bf1922bb8f_master.jpg" alt="">
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Thuốc trừ sâu</a>
+                            </div>
+                        </div>
+                        <div class="prd prd-1">
+                            <div class="ctg_i">
+                                <img style="width: 100%; height: 100%; border-radius: 5px;" src="https://cf.shopee.vn/file/fa40918ea31d00908839ad81397b80ab" alt="">
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Thuốc diệt cỏ</a>
+                            </div>
+                        </div>
+                        <div class="prd prd-1">
+                            <div class="ctg_i">
+                                <img style="width: 100%; height: 100%;" src="http://lavamix.vn/upload/products/phan-huu-co-sinh-hoc-minro-dang-vien-no.jpg" alt="">
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Phân hữu cơ</a>
+                            </div>
+                        </div>
+                        <div class="prd prd-1">
+                            <div class="ctg_i">
+                                <img style="width: 100%; height: 100%;" src="https://cf.shopee.vn/file/fec1f963c40aaecef7496b05d69cd945" alt="">
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Phân vô cơ</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="prod">
+                        <div class="prd prd-2">
+                            <div class="ctg_i">
+                                <img style="width: 100%; height: 100%;" src="https://thapxanh.com/images/thumbs/0022727_dau-bec-tuoi-canh-dap-xoay-360-ms-5022a-sung-phun-tuoi-cay-12-ren-21-sprinkler-gun.jpeg" alt="">
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Béc tưới tự động</a>
+                            </div>
+                        </div>
+                        <div class="prd prd-2">
+                            <div class="ctg_i">
+                             <img style="width: 100%; height: 100%;" src="https://bizweb.dktcdn.net/100/332/983/products/cat-co-honda-gx35-osaika-os26.jpg?v=1601435326517" alt="">  
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Máy cắt cỏ</a>
+                            </div>
+                        </div>
+                        <div class="prd prd-2">
+                            <div class="ctg_i">
+                                <img style="width: 100%; height: 100%;" src="https://dungcunongnghiep.vn/files/sanpham/918/1/jpg/hop-dung-cu-lam-vuon-lon-10-mon-xanh-hm216.jpg" alt="">
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Dụng cụ làm vườn</a>
+                            </div>
+                        </div>
+                        <div class="prd prd-2">
+                            <div class="ctg_i">
+                                <img style="width: 100%; height: 100%;" src="https://salt.tikicdn.com/cache/400x400/ts/product/d4/6c/32/0a1453d43e1a9d2e967b8b116b42381e.jpg" alt="">
+                            </div>
+                            <div class="ctg_n">
+                                <a href="">Đất trồng cây</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div></div>
+            </div>
+            <div id="content2">
+                xu huong tim kiem
+            </div>
+            <div id="content3">
+                day la top san pham ban chay
+            </div>
+            <div id="content4">
+                day la top san pham moi nhat
+            </div>
+            <div id="content5">
+                cac bai viet duoc doc nhieu
+            </div>
+        </div>  
         <!-- End content -->
 
         <!-- footer -->
