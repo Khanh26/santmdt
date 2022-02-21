c4d82c609b12d1cd661c44605fe6512cfe302a20
thinh0910 <doanphuocthinh0910@gmail.com>
2022-02-21 23:58:14 +0700

    Tin Tuc

---
+++ tintuc/tintuc.php
@@ -0,0 +1,83 @@
+<head>
+<style>
+.tintuc {
+  width: 100%;
+  display: flex;
+}
+
+div {
+  border: 0px solid black;
+  margin: 15px 15px;
+  background-color: rgb(239, 242, 243);
+  padding : 10px;
+  width: 800px;
+  height: 220px;
+  text-align: justify;
+}
+
+p.img {
+    text-align: center;
+}
+
+.fill {object-fit: fill;}
+.contain {object-fit: contain;}
+.cover {object-fit: cover;}
+.scale-down {object-fit: scale-down;}
+.none {object-fit: none;}
+
+.clearfix::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+.img1 {
+  float: left;
+}
+
+</style>
+</head>
+<body>
+
+<div calss="tintuc">
+<h1><center>Bài Viết Mới Nhất</center></h1>
+
+<div class="clearfix">
+  <p class="cover img1"><img src="images/hoa-mai-vang.jpg" alt="" style="width:300px;height:200px"></p>
+  <br>
+  <br>
+  &emsp;Mai có xuất xứ từ cây hoang dại, 
+  có khả năng thích ứng tốt với điều kiện khí hậu nhiệt đới, 
+  đặc biệt với khí hậu miền Nam. Cây mai sinh trưởng và phát triển mạnh, 
+  có tuổi thọ cao và nếu được chăm sóc chu đáo sẽ cho hoa nhiều và có màu sắc đẹp. 
+  Cây mai mỗi năm rụng lá một lần vào cuối mùa Đông (tháng 1 – tháng 2 Dương lịch) 
+  và nở hoa vào đầu mùa Xuân, chỉ riêng mai Tứ Qúy là nở hoa quanh năm.
+<a href="tintuc(hoamai).html">Xem Thêm</a>
+</div>
+<hr>
+
+<div class="clearfix">
+  <p class="cover img1"><img src="images/cay-xoai.jpg" alt="" style="width:300px;height:200px"></p>
+  <br>
+  <br>
+  &emsp;
+  <a href="tintuc(xoai).html">Xem Thêm</a>
+</div>
+<hr>
+
+<div class="clearfix">
+  <p class="cover img1"><img src="images/ky_thuat_trong_ca_phe.jpg" alt="" style="width:300px;height:200px"></p>
+  <br>
+  <br>
+  &emsp;Giới thiệu quy trình kỹ thuật trồng cà phê, 
+  hướng dẫn chăm sóc cây cà phê cho năng suất cao và ổn định. 
+  Ngoài việc lựa chọn giống cà phê đạt tiêu chuẩn, có xuất xứ rõ ràng, 
+  thì kỹ thuật trồng cũng quyết định không nhỏ đến sinh trưởng 
+  và năng suất của vườn cà phê. 
+  Hy vọng những thông tin sau sẽ bổ ích đối với bà con.
+  <a href="tintuc(CF).html">Xem Thêm</a>
+</div>
+<hr>
+</head>
+</div>
+
