972f0099bce6f3baade79d65016bd75686729c99
thinh0910 <99789907+thinh0910@users.noreply.github.com>
2022-02-21 23:17:33 +0700

    Update index.php

--- tintuc/index.php
+++ tintuc/index.php
@@ -16,7 +16,57 @@
         <!-- End header -->
 
         <!-- Content -->
-            <!-- Code trang chu o day -->
+        <div calss="tintuc_1">
+        <h1><center>Bài Viết Mới Nhất</center></h1>
+        
+        <div class="clearfix">
+            <p class="cover img1"><img src="http://localhost/santmdt/assets/img/hoa-mai-vang.jpg" alt="" style="width:300px;height:200px"></p>
+            <br>
+            <br>
+            &emsp;Mai có xuất xứ từ cây hoang dại, 
+            có khả năng thích ứng tốt với điều kiện khí hậu nhiệt đới, 
+            đặc biệt với khí hậu miền Nam. Cây mai sinh trưởng và phát triển mạnh, 
+            có tuổi thọ cao và nếu được chăm sóc chu đáo sẽ cho hoa nhiều và có màu sắc đẹp. 
+            Cây mai mỗi năm rụng lá một lần vào cuối mùa Đông (tháng 1 – tháng 2 Dương lịch) 
+            và nở hoa vào đầu mùa Xuân, chỉ riêng mai Tứ Quý là nở hoa quanh năm.
+            <a href="tintuc(hoamai).html">
+                <br>
+                Xem Thêm</a>
+        </div>
+        <hr>
+
+        <div class="clearfix">
+            <p class="cover img1"><img src="http://localhost/santmdt/assets/img/cay-xoai-6.jpg" alt="" style="width:300px;height:200px"></p>
+            <br>
+            <br>
+            &emsp;Mai có xuất xứ từ cây hoang dại, 
+            có khả năng thích ứng tốt với điều kiện khí hậu nhiệt đới, 
+            đặc biệt với khí hậu miền Nam. Cây mai sinh trưởng và phát triển mạnh, 
+            có tuổi thọ cao và nếu được chăm sóc chu đáo sẽ cho hoa nhiều và có màu sắc đẹp. 
+            Cây mai mỗi năm rụng lá một lần vào cuối mùa Đông (tháng 1 – tháng 2 Dương lịch) 
+            và nở hoa vào đầu mùa Xuân, chỉ riêng mai Tứ Quý là nở hoa quanh năm.
+            <a href="tintuc(xoai).html">
+                <br>
+                Xem Thêm</a>
+        </div>
+        <hr>
+
+        <div class="clearfix">
+            <p class="cover img1"><img src="http://localhost/santmdt/assets/img/ky_thuat_trong_ca_phe.jpg" alt="" style="width:300px;height:200px"></p>
+            <br>
+            <br>
+            &emsp;Giới thiệu quy trình kỹ thuật trồng cà phê, 
+            hướng dẫn chăm sóc cây cà phê cho năng suất cao và ổn định. 
+            Ngoài việc lựa chọn giống cà phê đạt tiêu chuẩn, có xuất xứ rõ ràng, 
+            thì kỹ thuật trồng cũng quyết định không nhỏ đến sinh trưởng 
+            và năng suất của vườn cà phê. 
+            Hy vọng những thông tin sau sẽ bổ ích đối với bà con.
+            <a href="tintuc(CF).html">
+                <br>
+            Xem Thêm</a>
+        </div>
+        <hr>
+        </div>
         <!-- End content -->
 
         <!-- footer -->
