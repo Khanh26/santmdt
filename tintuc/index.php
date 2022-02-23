38cd59b364ef130229770ec6ef8c6fb51365a5e7
Khanh26 <trongkhanh2014@gmail.com>
2022-02-23 14:23:10 +0700

    pull - check commit

--- tintuc/index.php
+++ tintuc/index.php
@@ -5,12 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.4/css/all.css" integrity="sha384-DyZ88mC6Up2uqS4h/KRgHuoeGwBcD4Ng9SiP4dIRy0EXTlnuz47vAwmeGwVChigm" crossorigin="anonymous">
-<<<<<<< HEAD
     <link rel="stylesheet" href="http://localhost/santmdt/assets/css/layouts.css">
-=======
-    <link rel="stylesheet" href="http://localhost/santmdt/assets/css/style.css">
-    <link rel="stylesheet" href="http://localhost/santmdt/assets/css/tintuc.css">
->>>>>>> 4cacfa5 (Tin Tuc)
     <title>Trang chủ - Sàn Thương mại số 1 Việt Nam</title>
 </head>
 
@@ -21,7 +16,6 @@
         <!-- End header -->
 
         <!-- Content -->
-<<<<<<< HEAD
         <div calss="tintuc_1">
         <h1><center>Bài Viết Mới Nhất</center></h1>
         
@@ -73,9 +67,6 @@
         </div>
         <hr>
         </div>
-=======
-        <?php include ('tintuc.php');?>
->>>>>>> 4cacfa5 (Tin Tuc)
         <!-- End content -->
 
         <!-- footer -->
