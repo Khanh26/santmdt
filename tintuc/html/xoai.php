e48f2c3d5df080fc66679cb48a929374480f8458
thinh0910 <doanphuocthinh0910@gmail.com>
2022-05-19 12:44:12 +0700

    MEOW

--- tintuc/html/xoai.php
+++ tintuc/html/xoai.php
@@ -14,72 +14,193 @@
         }
     </style>
     <div class="about">
-        <p class="caption_about"><center><h2>QUY TRÌNH TRỒNG VÀ CHẮM SÓC CÂY CÀ PHÊ ĐÚNG KỸ THUẬT</h2></center></p>
+        <p class="caption_about"><center><h2>QUY TRÌNH TRỒNG VÀ CHẮM SÓC CÂY XOÀI ĐÚNG KỸ THUẬT</h2></center></p>
         <p>
-            Giới thiệu quy trình kỹ thuật trồng cà phê, hướng dẫn chăm sóc cây cà phê cho năng suất cao và ổn định. 
-            Ngoài việc lựa chọn giống cà phê đạt tiêu chuẩn, có xuất xứ rõ ràng, 
-            thì kỹ thuật trồng cũng quyết định không nhỏ đến sinh trưởng và năng suất của vườn cà phê. 
-            Hy vọng những thông tin sau sẽ bổ ích đối với bà con.
+            Xoài là giống hoa quả nhiệt đới, đặc biệt phù hợp với điều kiện thời tiết nước ta nên cây trồng thường sinh trưởng, 
+            phát triển tốt cho năng suất và chất lượng cao. 
+            Do đó, đây là một trong những giống cây ăn quả được trồng phổ biến nhất hiện nay. 
+            Tuy nhiên, để có thể khai thác tối đa giá trị mà cây xoài mang lại, bà con cần nắm vững kiến thức, 
+            kỹ thuật trồng xoài và chăm sóc cây xoài.
         </p>
         <p class="img"><img src="http://localhost/santmdt/public/img/cay-xoai-6.jpg" alt=""></p>
         <p>
-            <h3>Cây cà phê yêu cầu khí hậu và đất trồng như thế nào?</h3>
-            <li><b>Đối với cà phê mít, cà phê vối:</b> khí hậu phù hợp là khí hậu nóng ẩm, mưa nhiều (2000mm/năm trở lên).
+            <h3>1. Điều kiện môi trường giúp cây xoài phát triển tốt nhất</h3>
+            <li><b>Yêu cầu về đất trồng</b>
                 <br>
-                &ensp;&emsp; Nhiệt độ từ 24-26 độ C
-            </li>
-            <br>
-
-            <li><b>Đối với cà phê chè:</b> Thích hợp với khí hậu lạnh hơn từ 16-22 độ C, lượng mưa từ 1700 – 2000mm.
+                Đặc tính của đất được hình thành bởi các loại chất và tỉ lệ của chúng trong đất trồng. 
+                Đây cũng chính là những loại chất dinh dưỡng mà cây xoài sẽ hấp thụ trong suốt quá trình phát triển và ra quả. 
+                Vì vậy, chỉ có những loại đất trồng phù hợp mới có thể giúp cây phát triển khỏe mạnh, cho nhiều trái và trái có chất lượng tốt.
                 <br>
-                &ensp;&emsp; Ở Việt Nam vùng Lâm Đồng trồng cà phê chè là phù hợp nhất.
+                Các loại đất phù hợp để trồng xoài bao gồm đất pha cát, đất đỏ bazan, đất vàng, 
+                đất phù sa, đất feralit, đất xám,… Trong đó, đất pha cát là loại đất có thể đem đến hiệu quả tốt nhất. 
+                Ở những khu vực trồng xoài, lớp đất canh tác phải có độ dày tối thiểu là 1.5m. 
+                Các mạch nước ngầm nên cách mặt trên 2.5m. Độ pH của đất nên được duy trì trong khoảng 5.5 – 7.0, 
+                nếu độ pH nằm ngoài khoảng này thì cần tiến hành các biện pháp cải tạo phù hợp trước khi trồng xoài.
             </li>
             <br>
 
-            <li><b>Cả 3 giống cà chè, cà vối và cà mít:</b> đều yêu cầu có sự phân chia 2 mùa mưa nắng rõ rệt, tạo điều kiện ức chế mầm hoa, 
+            <li><b>Thời tiết – Mùa vụ trồng xoài</b>
                 <br>
-                &ensp;&emsp; giúp cây ra hoa đậu quả tập trung hơn
+                    Vì xoài là giống cây nhiệt đới nên phát triển tốt ở mức nhiệt cao, 
+                    có khả năng chịu nhiệt lên đến 40 – 45oC  tùy giống. 
+                    Tuy nhiên, nhiệt độ lý tưởng nhất cho sự phát triển của xoài là khoảng 23 – 28oC. 
+                    Nên tránh việc trồng xoài ở những vùng mưa nhiều, thời tiết lạnh quanh năm, 
+                    hoặc bắt đầu mùa vụ vào đúng thời điểm thời tiết diễn biến cực đoan như sương muối, 
+                    rét đậm, rét hạn.
+                <br>
+                    Bạn nên tiến hành trồng xoài vào giai đoạn đầu mùa mưa vì thời điểm này thời tiết thường khá phù hợp, 
+                    lại có thể tranh thủ lượng nước tưới tự nhiên. Bên cạnh đó, 
+                    trong trường hợp bạn hoàn toàn chủ động về điều kiện tưới tiêu thì có thể trồng xoài quanh năm.
             </li>
             <br>
 
+            <h3>2. Chuẩn bị trồng xoài</h3>
             <li>
-                <b>Yêu cầu về đất trồng:</b> Đất trồng cà phê phải là loại đất giàu dinh dưỡng, thoát nước tốt, tầng canh tác sâu 0,8 – 1m. 
-                <br>&ensp;&emsp;Các loại đất xám, đất thịt pha, đất đỏ bazan đều có thể trồng cà phê, nhưng nhìn chung đất đỏ bazan là phù hợp nhất. 
-                <br>&ensp;&emsp;Cho năng suất cao, cây sinh trưởng mạnh
+                <b>Lựa chọn giống</b>
+                <br>
+                Khi lựa chọn giống xoài, bạn nên cân nhắc các yếu tố như điều kiện trồng, 
+                chăm sóc, đặc thù của giống cây, nhu cầu của thị trường, giá cả thu mua, 
+                thời gian bảo quản,… Đây là những yếu tố giúp việc trồng xoài trở nên đơn giản hơn nhưng lại đem về giá trị kinh tế cao.
+                <br>
+                Những loại xoài đang được trồng phổ biến nhất ở nước ta như giống xoài Đài Loan, 
+                xoài Thái, xoài Úc, xoài Tứ Quý,… có khả năng tiêu thụ khá tốt và có nhiều tiềm năng xuất khẩu, 
+                cung cấp cho những thị trường nước ngoài.
             </li>
             <br>
 
-            <li>
-                <b>Đất cải tạo cà phê lâu năm:</b> Cần được cày xới, bổ sung phân hữu cơ (phân chuồng hoai mục, phân xanh). 
-                <br>&ensp;&emsp;Và canh tác ít nhất 2 vụ màu để giảm thiểu tuyến trùng, mầm bệnh
-            </li>
+            <h3>Chuẩn bị đất trồng</h3>
+            Khu vực đất trồng xoài nên được kiểm tra và xử lý phù hợp để tạo điều kiện tốt nhất cho sự phát triển của cây. 
+            Một số công việc cần thực hiện:
             <br>
 
             <li>
-                <b>Yêu cầu về gió và ánh sáng mặt trời:</b> Cây ưa thích môi trường kín gió, ánh sáng tán xạ. 
-                <br>&ensp;&emsp;Ánh sáng trực tiếp cây vẫn phát triển nhưng thường chống chọi với mùa khô hạn kém hơn. 
-                <br>&ensp;&emsp;Do đó yêu cầu khi canh tác cây cà phê, nhất định phải trồng hệ thống cây che bóng, cây chắn gió xung quanh 
-                <br>&ensp;&emsp;hoặc chắn gió giữa các hàng trong giai đoạn kiến thiết
+                Dọn toàn bộ cỏ, rác trên bề mặt.
             </li>
-            <br>
-
-            <h3>Mật độ trồng cà phê</h3>
-            <li>
-                <b>Cà phê vối:</b> Mật độ trồng phổ biến là 3m x 3m hoặc 2,8 x 2,8m đối với đất bằng phẳng (tương đương 1100 cây/hecta). 
-                <br>&ensp;&emsp;Đất dốc nhiều trồng 3m x 2,5m (tương đương 1300 cây/hecta). 
-                <br>&ensp;&emsp;Ngoài ra tùy theo đặc điểm của giống cà phê, mật độ trồng cũng có thể khác nhau.
-            </li>
-            <br>
 
             <li>
-                Cà phê mít mật độ trồng là 5×5 hoặc 7×7 m (khoảng 700 cây/hecta)
+                Xới đất để tạo độ tơi xốp.
             </li>
-            <br>
 
             <li>
-                Cà phê chè mật độ trồng dày nhất 1m x 2m (khoảng 4000 cây/hecta)
+                Thực hiện các biện pháp cải tạo đất: sử dụng các phương pháp để kiểm tra, 
+                cải thiện độ pH cho phù hợp, trồng thêm một số loại cây họ đậu để tăng độ đạm.
             </li>
 
+            <li>
+                Đào các hố trồng với kích thước 60x60x60 cm, 
+                bón vào tứng hố 20 – 30 kg phân chuồng +1 kg lân + 0.1 kg Kali + 0.5 kg vôi bột và 
+                tưới một lượng nước vừa đủ sau đó lấp đất lại.
+            </li>
+            Ngoài ra, cần lưu ý rằng nếu trước đó, 
+            khu đất được dùng để trồng các loại cây lâu năm khác thì nên để đất được nghỉ trong tối thiểu nửa năm rồi mới bắt đầu trồng xoài.
+
+            <h3>3. Kỹ thuật trồng xoài</h3>
+                Trên thực tế, có rất nhiều phương pháp và kỹ thuật trồng xoài khác nhau, 
+                mỗi phương pháp đều có những lợi thế riêng. Ở bài viết này, bạn sẽ được tiếp cận với hai kỹ thuật trồng xoài phổ biến nhất.
+                <br>
+                <br>
+                <b>Phương pháp xuống giống cây con</b>
+                <br>
+                Phương pháp xuống giống cây con: các hạt xoài giống sẽ được trồng và chăm sóc trong những điều kiện lý tưởng, 
+                phù hợp với giai đoạn đầu phát triển của cây. Sau đó, khi cây đã đạt độ cao tiêu chuẩn thì mới đem trồng ở các vùng đất canh tác được chuẩn bị trước đó. 
+                <br>
+                <br>
+                <b>Các bước thực hiện:</b>
+                <br>
+                <li>
+                Ở vị trí hố trồng xoài được chuẩn bị trước đó, đào một hố mới có đường kính lớn hơn bầu ươm từ 10 – 15 cm.
+                </li>
+                <li>
+                Cắt lớp nilon bọc ngoài bầu ươm, sử dụng dụng cụ phù hợp và thao tác nhẹ nhàng để tránh làm vỡ bầu hoặc đứt rễ.
+                </li>
+                Đặt cây xoài non vào những vị trí được đào sẵn, giữ cổ rễ nằm ngang với bề mặt đất trồng.
+                <li>
+                Vun đất vào xung quanh, dùng lực nén nhẹ để đảm bảo lượng đất được lấp vừa đủ, 
+                không làm cây bị đổ, cũng không làm hố đất bị nén quá chặt, làm nước khó lưu thông, ảnh hưởng đến khả năng hấp thụ của dễ.
+                </li>
+                Vun thêm một lớp đất cao vào gốc để tránh tính trạng úng nước.
+                <li>
+                Tưới nước và có thể dùng thêm một số loại rơm rạ, cỏ, trấu  phủ lên xung quanh để giữ độ ẩm cho cây vào mùa khô.
+                </li>
+                <li>
+                Cắm cọc cố định cây.
+                </li>
+                <br>
+
+
+
+                <b>Phương pháp ghép nhân giống</b>
+                <br>
+                Phương pháp ghép nhân giống: ghép một bộ phận của giống xoài bạn định trồng vào các gốc xoài địa phương có sẵn để rút ngắn thời gian ra quả và tăng sức chống chịu của cây. 
+                Phương pháp này nên được thực hiện vào lúc khí hậu mát mẻ, 
+                ít mưa để tạo điều kiện thuận lợi nhất cho sự phát triển của cây xoài
+                <br>
+                <br>
+                <b>Thực hiện ghép cây theo những bước sau:</b>
+                <br>
+                <li>
+                    Chuẩn bị các loại dụng cụ cần thiết như dao ghép, nilon, dây buộc.
+                </li>
+                <li>
+                    Lựa chọn cành ghép và gốc chất lượng: chọn cành bánh tẻ dài khoảng 40 cm, sạch sâu bệnh, gốc ghép khỏe mạnh có khả năng thích ứng cao.
+                </li>
+                    Dùng dao cắt một lát dài  5 – 6 cm ở vị trí ghép trên gốc và ở đuôi cành ghép.
+                <li>
+                    Ghép cành vào gốc, điều chỉnh vị trí phù hợp để hai mối ghép ăn khớp tương đối với nhau.
+                </li>
+                    Dùng nilon và dây bọc lại để tránh bị dính nước làm hỏng mối ghép, có thể dùng thêm sáp hoặc nến giữa các đầu nilon.
+                <li>
+                    Liên tục quan sát và loại bỏ những mối ghép không phát triển tốt, sau khoảng  2 – 4 tháng khi vết ghép dính liền thì có thể tháo các dụng cụ hỗ trợ xung quanh.
+                </li>
+                <br>
+
+                <h3>4. Chăm sóc cây sau khi trồng</h3>
+                <b>Tưới Nước</b>
+                <br>
+                Ở giai đoạn mới trồng, cứ cách 3 ngày cần tưới nước cho cây xoài một lần. 
+                Về sau, thời gian giữa hai lần tưới sẽ liên tục được kéo dãn. 
+                Thêm vào đó, có thể dựa vào điều kiện thời tiết để điều chỉnh thời gian tưới phù hợp, 
+                đảm bảo độ ẩm cho đất trồng.
+                <br>
+                Khi cây bắt đầu cho ra quả, bộ rễ sinh trưởng đầy đủ, 
+                ăn sâu vào đất thì bạn không cần phải chú ý tưới tới thời gian tưới nước cho cây. 
+                Chỉ cần tưới thêm nước nếu thời gian đó nắng nóng mạnh, khô hạn kéo dài.
+                <br>
+                <br>
+                <b>Làm Cỏ</b>
+                <br>
+                Bạn cần chú ý và loại bỏ các cây dại mọc xung quanh gốc để tránh tính trạng cạnh tranh dinh dưỡng, 
+                khiến cây xoài kém phát triển. Việc này nên được thực hiện thường xuyên khi cây còn non, 
+                nhất là vào mùa mưa, không nên để cây dại mọc quá nhiều, quá rậm.
+                <br>
+                <br>
+                <b>Cắt tỉa cành và tạo tán</b>
+                <br>
+                Cắt tỉa cành và tạo tán được thực hiện với nhiều mục đích như hãm chiều cao giúp cây tập trung dinh dưỡng phát triển cành lá và quả, 
+                loại bỏ những cành sâu già cỗi để giúp cây phát triển khỏe mạnh, loại bỏ những cành mọc quá sát, 
+                những cành khó ra quả, các cành vụn từ mùa thu hoạch trước,…
+                <br>
+                Việc cắt tỉa nên được thực hiện 1-2 lần mỗi năm, tùy theo tính hình phát triển thực tế của cây trồng.
+                <br>
+                <br>
+                <b>Bón Phân</b>
+                <br>
+                    Sử dụng phân bón là phương pháp đơn giản nhất giúp kịp thời bổ sung các chất dinh dưỡng cần thiết cho sự phát triển của cây vào từng giai đoạn.
+                <br>
+                <li>
+                    Giai đoạn kiến thiết: sử dụng các loại phân bón NPK để kích thích sự phát triển của rễ, thân, cành. Mỗi năm nên bón từ 2 – 4 lần để đảm bảo dinh dưỡng được cung cấp đầy đủ.
+                </li>
+                <li>
+                    Giai đoạn kinh doanh: lượng phân bón nên tăng lên 2-3 kg/cây/năm, bón thành 3 – 5 đợt mỗi. Đặc biệt, nên bổ sung nhiều kali để gia tăng khả năng đậu quả và nâng cao chất lượng trái.
+                </li>
+                <li>
+                Giai đoạn nuôi trái: tăng hàm lượng Kali để tăng kích cỡ và phẩm chất trái
+                </li>
+                <br>
+                Bên cạnh việc cân nhắc đến thời điểm bón phân, 
+                bạn cũng cần tìm kiếm những đơn vị cung cấp phân bón chất lượng như công ty phân bón Hà Lan. 
+                Bạn có thể lựa chọn nhiều loại phân bón NPK với các công thức khác nhau NPK 15-15-15, 
+                NPK 17-17-17, NPK 12-12-18,… cho từng giai đoạn phát triển của cây.
         </p>
 
     </div>
